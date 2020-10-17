3d2f0f9a62fb1608647da4956abfca6a24443f9c
kushal <kushaldeveloper@gmail.com>
2020-10-17 17:03:02 -0600

    switch to slickdeals

--- puppeteer.js
+++ puppeteer.js
@@ -1,29 +1,8 @@
 'use strict';
 const puppeteer = require('puppeteer');
 
-crawlPage("https://www.nytimes.com/", "frontpage");
-crawlPage("https://www.nytimes.com/section/world", "world");
-crawlPage("https://www.nytimes.com/section/us", "us");
-crawlPage("https://www.nytimes.com/section/politics", "politics");
-crawlPage("https://www.nytimes.com/section/nyregion", "nyregion");
-crawlPage("https://www.nytimes.com/section/business", "business");
-crawlPage("https://www.nytimes.com/section/opinion", "opinion");
-crawlPage("https://www.nytimes.com/section/technology", "technology");
-crawlPage("https://www.nytimes.com/section/science", "science");
-crawlPage("https://www.nytimes.com/section/health", "health");
-crawlPage("https://www.nytimes.com/section/sports", "sports");
-crawlPage("https://www.nytimes.com/section/arts", "arts");
-crawlPage("https://www.nytimes.com/section/books", "books");
-crawlPage("https://www.nytimes.com/section/style", "style");
-crawlPage("https://www.nytimes.com/section/food", "food");
-crawlPage("https://www.nytimes.com/section/travel", "travel");
-crawlPage("https://www.nytimes.com/section/magazine", "magazine");
-crawlPage("https://www.nytimes.com/section/t-magazine", "times-magazine");
-crawlPage("https://www.nytimes.com/section/realestate", "real-estate");
-crawlPage("https://www.nytimes.com/section/video", "video");
-
-
-
+crawlPage("https://slickdeals.net/", "front-page");
+crawlPage("https://slickdeals.net/deals/", "popular-deals");
 
 function delay(time) {
     return new Promise(function (resolve) {
@@ -51,7 +30,7 @@ function crawlPage(url, prefix) {
             width: 1920,
             height: 1080
         });
-        
+
         await page.goto(url, {
             waitUntil: 'networkidle0',
             timeout: 0
@@ -62,19 +41,21 @@ function crawlPage(url, prefix) {
         for (let i = 0; i < addresses.length; i++) {
             console.log(`Now serving ${i} of ${addresses.length}: ${addresses[i]}`);
             try {
-                await page.goto(addresses[i], { waitUntil: "networkidle0", timeout: 0 });
+                if (addresses[i].startsWith("http") === true) {
+                    await page.goto(addresses[i], { waitUntil: "networkidle0", timeout: 300000 });
 
-                const watchDog = page.waitForFunction(() => 'window.status === "ready"', { timeout: 0 });
-                await watchDog;
+                    const watchDog = page.waitForFunction(() => 'window.status === "ready"', { timeout: 300000 });
+                    await watchDog;
 
-                await page.screenshot({
-                    path: `screenshots/${prefix}-${i}.png`,
-                    fullPage: true
-                });
-                await page.screenshot({
-                    path: `screenshots/${prefix}-${i}-fold.png`,
-                    fullPage: false
-                });
+                    await page.screenshot({
+                        path: `screenshots/${prefix}-${i}.png`,
+                        fullPage: true
+                    });
+                    await page.screenshot({
+                        path: `screenshots/${prefix}-${i}-fold.png`,
+                        fullPage: false
+                    });
+                }
             } catch (error) {
                 console.error(error);
             } finally {
