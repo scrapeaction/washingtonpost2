9ed850f9bec7ce25b1994896771e047598e5e30c
kushal <kushaldeveloper@gmail.com>
2020-10-17 14:29:08 -0600

    use element scroll into view
    https://stackoverflow.com/a/60713777/227646

--- puppeteer.js
+++ puppeteer.js
@@ -40,44 +40,26 @@ function crawlPage() {
             console.log(addresses[i]);
             try {
                 await page.goto(addresses[i], { waitUntil: "networkidle0", timeout: 0 });
-                // Get scroll width and height of the rendered page and set viewport
-                const bodyWidth = await page.evaluate(() => document.body.scrollWidth);
-                const bodyHeight = await page.evaluate(() => document.body.scrollHeight);
-                await page.setViewport({ width: bodyWidth, height: bodyHeight });
 
-                // Set the viewport before scrolling
-                await page.setViewport({ width: 1366, height: 768 });
+                await page.evaluate(async () => {
+                    // Scroll down to bottom of page to activate lazy loading images
+                    document.body.scrollIntoView(false);
 
-                // Get the height of the page after navigating to it.
-                // This strategy to calculate height doesn't work always though. 
-                const bodyHandle = await page.$('body');
-                const { height } = await bodyHandle.boundingBox();
-                await bodyHandle.dispose();
+                    // Wait for all remaining lazy loading images to load
+                    await Promise.all(Array.from(document.getElementsByTagName('img'), image => {
+                        if (image.complete) {
+                            return;
+                        }
 
-                // Scroll viewport by viewport, allow the content to load
-                const calculatedVh = page.viewport().height;
-                let vhIncrease = 0;
-                while (vhIncrease + calculatedVh < height) {
-                    // Here we pass the calculated viewport height to the context
-                    // of the page and we scroll by that amount
-                    await page.evaluate(_calculatedVh => {
-                        window.scrollBy(0, _calculatedVh);
-                    }, calculatedVh);
-                    await waitFor(300);
-                    vhIncrease = vhIncrease + calculatedVh;
-                }
-
-                // Setting the viewport to the full height might reveal extra elements
-                await page.setViewport({ width: 1366, height: calculatedVh });
-
-                // Scroll back to the top of the page by using evaluate again.
-                await page.evaluate(_ => {
-                    window.scrollTo(0, 0);
+                        return new Promise((resolve, reject) => {
+                            image.addEventListener('load', resolve);
+                            image.addEventListener('error', reject);
+                        });
+                    }));
                 });
 
                 await delay(4000);
                 console.log(`waited for four seconds`);
-
                 await page.screenshot({
                     path: `screenshots/screenshots-${i}.png`,
                     fullPage: true
